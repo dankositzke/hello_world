99032b894ec54401567f4d384687d20273f3d421
Dan Kositzke <dankositzke2050@gmail.com>
2021-11-19 19:22:47 -0500

    Revert "Added call to undefined_function() within welcome_message()"
    Reason for rollback: the code is calling an undefined function.
    
    This reverts commit b98c62f734e8b515b08fdf7e3bf1b17f4d91e341.

--- welcome.py
+++ welcome.py
@@ -1,8 +1,6 @@
 def welcome_message():
     name = input("Hello! What is your name? ")
     print('What a beautiful name :)')
-    if undefined_function():
-        print('Good work today')
     print('Welcome ' + name + '.')
 
 welcome_message()
