e4f83af3e133c9878b46b66f27ba1376cf10a81c
Dan Kositzke <dankositzke2050@gmail.com>
2021-11-19 20:20:46 -0500

    Add comment to welcome_message()

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 def welcome_message():
+    """This function gives the user a friendly welcome"""
     name = input("Hello! What is your name? ")
-    print('What a beautiful name :)')
-    print('Welcome ' + name + '.')
+    print("What a beautiful name :)")
+    print("Welcome " + name + ".")
+
 
 welcome_message()
