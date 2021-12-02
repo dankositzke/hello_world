100a938b3285bfde99e6f3825184475357ee3046
Dan Kositzke <dankositzke2050@gmail.com>
2021-12-01 20:51:23 -0500

    Improved the hello.py file

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def welcome_message():
     """This function gives the user a friendly welcome"""
-    name = input("Hello! Who the heck are you? ")
-    print("What a beautiful name :)")
+    name = input("Hello there! And what is your name? ")
+    print("What a beautiful name :)\n")
     print("Welcome " + name + ".")
 
 
